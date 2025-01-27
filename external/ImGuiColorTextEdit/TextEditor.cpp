f2bd5a0f8c6b79beb226b97bd565d292bfb81600
Arvid Nilsson <arvid2.nilsson@gmail.com>
2025-01-27 09:55:04 +0100

    More TextEditor bug fixes

--- external/ImGuiColorTextEdit/TextEditor.cpp
+++ external/ImGuiColorTextEdit/TextEditor.cpp
@@ -2190,16 +2190,14 @@ void TextEditor::EnsureCursorVisible()
     float scrollY = ImGui::GetScrollY();
 
     ImVec2 contentRegionAvail = ImGui::GetContentRegionAvail();
-    //auto height = ImGui::GetWindowHeight();
-    //auto width = ImGui::GetWindowWidth();
     auto height = contentRegionAvail.y;
     auto width = contentRegionAvail.x;
 
     auto top = 1 + (int)ceil(scrollY / mCharAdvance.y);
     auto bottom = (int)ceil((scrollY + height) / mCharAdvance.y);
 
-    auto left = (int)ceil(scrollX / mCharAdvance.x);
-    auto right = (int)ceil((scrollX + width) / mCharAdvance.x);
+    auto left = (int)ceil(scrollX);
+    auto right = (int)ceil(scrollX + width);
 
     auto pos = GetActualCursorCoordinates();
     auto len = TextDistanceToLineStart(pos);
